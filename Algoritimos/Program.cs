94718706aa5ce538f4b38e5593f0fac7b87d039a
ELINALDO-NS <elinaldo_nascimento@outlook.com>
2023-11-03 14:47:52 -0300

    Primeiro Commit

---
+++ Algoritimos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+  var a = TwoSum(new int[] { 8, 5, 7 }, 12);
+
+
+            Console.WriteLine(a[0]+" "+ a[1]);
+            Console.ReadKey();
+
+             int[] TwoSum(int[] nums, int target)
+            {
+                // Crie um dicionário para armazenar números vistos e seus índices
+                Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    // Calcule o complemento do número atual
+                    int complement = target - nums[i];
+
+                    // Se o complemento existir no dicionário, retorne seu índice e o índice atual
+                    if (numDict.ContainsKey(complement))
+                    {
+                        return new int[] { numDict[complement], i };
+                    }
+
+                    // Caso contrário, armazene o número atual e seu índice no dicionário
+                    numDict[nums[i]] = i;
+                }
+
+                // Se nenhuma solução for encontrada, retorne um array vazio ou manipule-o conforme necessário.
+                return new int[0];
+            }
+              Console.ReadKey();
